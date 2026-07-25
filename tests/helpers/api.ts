1b133db78eeeff63bb2b08ea47afd14eb96e132a
zunarvy19 <arvy977.gmail.com>
2026-07-26 00:55:30 +0700

    feat: implement ApiHelper class for automated integration testing

--- tests/helpers/api.ts
+++ tests/helpers/api.ts
@@ -1,6 +1,6 @@
 import { APIRequestContext, expect } from "@playwright/test";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "http://43.157.248.166:5001";
 
 export interface AuthResponse {
   token: string;
